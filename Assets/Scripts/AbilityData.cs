47690d81425df721ab5126c141e9dafd5b4dcc43
Mooouse <62959093+Mooouse@users.noreply.github.com>
2026-06-16 11:51:01 +0500

    Add AbilityData scriptable object

---
+++ Assets/Scripts/AbilityData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AbilityData
+{
+    public string abilityName;
+    public string description;
+    public int healthBonus;
+    public int damageBonus;
+    public int defenseBonus;
+    public float attackSpeedBonus;
+    public float stunChanceBonus;
+}
